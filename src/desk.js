9b6b4880b1298b4ecf4db45c88beefc50cc12a4e
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-09-16 15:57:48 -0400

    Added desk functionality

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,21 @@
 class Desk {
-  
+    constructor(length, width, height, isWhiteboard) {
+        this.length = length
+        this.width = width
+        this.height = height
+        this.isWhiteboard = isWhiteboard
+
+        this.content = ""
+    }
+
+    write(input) {
+        this.content += input
+    }
+
+    wipe() {
+        if (this.isWhiteboard)
+            this.content = ""
+    }
 }
 
 module.exports = Desk
