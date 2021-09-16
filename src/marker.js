fb95a6eb7cd9f72c562cb19327f1aa891d983882
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-09-16 16:14:26 -0400

    Added marker functionality

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,24 @@
 class Marker {
-  
+    constructor(size, color, remainingInk) {
+        this.size = size
+        this.color = color
+        this.remainingInk = remainingInk
+    }
+
+    write(whatToWrite) {
+        let whatIwrote = ""
+        for (let letter of whatToWrite) {
+            if (letter === " ") {
+                whatIwrote += " "
+            } else {
+                if (this.remainingInk > 0) {
+                    this.remainingInk -= 1
+                    whatIwrote += letter
+                }
+            }
+        }
+        return whatIwrote
+    }
 }
 
 module.exports = Marker
