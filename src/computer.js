b520cf81ddbc6d238a603b87f476cec0abf9d849
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-09-16 15:42:03 -0400

    Added computer functionality

--- src/computer.js
+++ src/computer.js
@@ -1,5 +1,17 @@
 class Computer {
-  
+    constructor(name, operatingSystem, processor, memory, graphics) {
+        this.name = name
+        this.operatingSystem = operatingSystem
+        this.processor = processor
+        this.memory = memory
+        this.graphics = graphics
+
+        this.on = false
+    }
+
+    power() {
+        this.on = !this.on
+    }
 }
 
 module.exports = Computer
