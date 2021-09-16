3cda95616d5b8dc9e9094a5c04bfca4f5857deed
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-09-16 16:42:18 -0400

    Added student functionality

--- src/student.js
+++ src/student.js
@@ -1,5 +1,35 @@
 class Student {
-  
+    constructor(name, skillLevel) {
+        this.name = name
+        this.skillLevel = skillLevel
+
+        this.assignments = []
+    }
+
+    study() {
+        if (this.skillLevel < 100) {
+            this.skillLevel += 1
+        }
+        return this;
+    }
+
+    doHomework(homework = null) {
+        if (homework === null) {
+            for (let assignment of this.assignments) {
+                if (assignment.skillLevel <= this.skillLevel) {
+                    assignment.completed = true
+                }
+            }
+            return
+        }
+
+        if (!homework.skillLevel || homework.skillLevel < 1 || homework.skillLevel > 100) {
+            return
+        }
+
+        homework.completed = homework.skillLevel <= this.skillLevel
+        this.assignments.push(homework)
+    }
 }
 
 module.exports = Student
